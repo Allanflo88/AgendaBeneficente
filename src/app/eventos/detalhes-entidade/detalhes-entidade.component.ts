b6cce0fec4dfc6d56729293b68d2f68463e09bdc
Allanflo88 <allanfloresdejesus@hotmail.com>
2018-12-08 15:03:32 -0200

    update detalhes entidade

--- src/app/eventos/detalhes-entidade/detalhes-entidade.component.ts
+++ src/app/eventos/detalhes-entidade/detalhes-entidade.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EntidadesService } from 'src/app/services/entidades.service';
 import { Entidade } from 'src/app/models/entidade';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detalhes-entidade',
@@ -13,7 +14,7 @@ export class DetalhesEntidadeComponent {
   
   entidade: Entidade
   serviceEntidade;
-  constructor(private route: ActivatedRoute, serviceEntidade: EntidadesService) {
+  constructor(private route: ActivatedRoute, serviceEntidade: EntidadesService, private sanitizer:DomSanitizer) {
     this.entidade = new Entidade();
     this.serviceEntidade = serviceEntidade;
     this.getEntidade();
@@ -22,5 +23,7 @@ export class DetalhesEntidadeComponent {
   getEntidade(){
     let id = this.route.snapshot.paramMap.get('id');
     this.entidade = this.serviceEntidade.getEntidade(id);
+    this.entidade.Endereco.CEP = this.entidade.Endereco.CEP.slice(0,5) + "-" +this.entidade.Endereco.CEP.slice(5)
+    this.entidade.Endereco.Localizacao = this.sanitizer.bypassSecurityTrustResourceUrl(this.entidade.Endereco.Localizacao )
   }
 }
